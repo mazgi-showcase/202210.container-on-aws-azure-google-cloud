dce9f70a8b6dac72968c9af8e2e23476059af9b0
Hidenori Matsuki <dev@mazgi.com>
2022-10-16 16:05:39 +0900

    add a comment for aws_apprunner_service.image_configuration.

--- provisioning/aws-container-202210/app-runner.tf
+++ provisioning/aws-container-202210/app-runner.tf
@@ -10,8 +10,10 @@ resource "aws_apprunner_service" "main" {
     # App Runner doesn't support automatic deployments for an image in an ECR Public repository.
     auto_deployments_enabled = false
     image_repository {
+      # If you don't specify image_configuration, AWS will apply image_configuration.port = 8080,
+      # and you'll occur a change the next time you'll plan and apply.
       image_configuration {
-        runtime_environment_variables = {}
+        runtime_environment_variables = { /* empty */ }
       }
       image_identifier      = "public.ecr.aws/aws-containers/hello-app-runner:latest"
       image_repository_type = "ECR_PUBLIC"
